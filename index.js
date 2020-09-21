e8ed35cffbd8ab9a039274f7fa137a7f537c7ccf
katrinamarquez <katrina.l.marquez@gmail.com>
2020-09-22 09:50:27 +1000

    Add in time func and tidy up HTML classes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("hello from the script!")
+
+// Append text to a p tag
+function showTime(){
+  // add time to p tag
+  let time = new Date();
+  let currentTime = time.getHours() + ":" + time.getMinutes();
+  let clockDisplay = document.getElementById("testTime");
+
+  // Time shows in console as a string
+  clockDisplay.innerText = currentTime; 
+}
+
+// When the page is loaded/refreshed
+window.onload = function display () {
+  // Everytime you add a new function make sure to call it down here
+  showTime();
+}
