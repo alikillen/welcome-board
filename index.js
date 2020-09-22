cc3387c757c74c399b3b85f4d64c5837da630c3c
alikillen <alisoneme1994@gmail.com>
2020-09-22 15:01:46 +1000

    background loading!

--- index.js
+++ index.js
@@ -1,73 +1,59 @@
 console.log("hello from the script!")
 
-// Append text to a p tag
-function showTime(){
+// appending text to a P tag
+function showTime() {
   // add time to p tag
   let time = new Date();
-  let currentTime = time.getHours() + ":" + time.getMinutes();
-  let clockDisplay = document.getElementById("testTime");
+  let currentTime = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
+  let clockDisplay = document.getElementById("test");
 
-  // Time shows in console as a string
-  clockDisplay.innerText = currentTime; 
+  clockDisplay.innerText = currentTime;
+};
+
+function getQuote() {
+  const quote = document.querySelector("#quote");
+  const author = document.querySelector("#author");
+
+  // Quote automatically loads when page is loaded
+  // window.addEventListener('load', getQuote);
+
+  fetch("http://api.quotable.io/random")
+  .then(res => res.json())
+  .then(data => {
+    quote.innerHTML = `"${data.content}"`;
+    author.innerHTML = `${data.author}`;
+  })
 }
 
 function getBackgroundImage() {
   fetch('https://api.unsplash.com/photos/random/?client_id=NqIsO3c4yryENpk6v__yQqDe8I_dMASVVrLTcba8X1Q')
-  // convert to json???
+  // get rid of secret key/dont upload
   .then(response => response.json())
   .then(function(data) {
-    console.log(data)
-  // Here you get the data to modify as you please
-      // document.body.style.backgroundImage = data;
-      // getting a 401 error - no API key
-
-      let photoUrl = data.urls.full
-      // does this need to more specific - object in postman with raw url
-      console.log(photoUrl)
-
-      // console.log(document.getElementById("wrapper"))
-
-      // var test = document.createElement('img')
-      // test.src = photoUrl
-      // console.log(test)
-      // quoteDiv = document.getElementById("quote")
-      // console.log(quoteDiv)
-      // quoteDiv.append(test)
-
-      // document.html.style.backgroundImage = "url(" + photoUrl + ")"
-
-      // jquery syntax
-      // $("wrapper").css("background-image", "url(" + photoUrl + ")");
-
-      // console.log(document.getElementById("wrapper"))
-
-      // document.getElementById("wrapper").style.backgroundImage = "url(" + photoUrl + ")"
-
+    // console.log(data)
     
-        let item = document.createElement('div');
-        item.classList.add('item');
-        item.innerHTML = `
-        <img class="random-image" src="${photoUrl}" alt="random"/>
-        `
-        document.body.appendChild(item);
+      let photoUrl = data.urls.full     
+      // console.log(photoUrl)
+      //   console.log(the_html)
+      document.getElementById('the_html').style.backgroundImage = "url(" + photoUrl + ")"
         })
-
-    
+   
 
   .catch(function(error) {
     console.log(error)
-  // If there is any error you will catch them here
   });   
 
 }
 
-// When the page is loaded/refreshed
+// do it all in one onload function in JS file
+// When the page is loaded/refreshed, calls all funcs
 window.onload = function display () {
   // Everytime you add a new function make sure to call it down here
   showTime();
+  getQuote();
   getBackgroundImage();
 }
 
 
-// do it all in one onload function in JS file
+
     
