7903865e11fe17aba8d94019e383507ba98bbce2
alikillen <alisoneme1994@gmail.com>
2020-09-22 10:07:22 +1000

    adding background func to js script

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 console.log("hello from the script!")
 
-// trigger this function on window load
+// Append text to a p tag
+function showTime(){
+  // add time to p tag
+  let time = new Date();
+  let currentTime = time.getHours() + ":" + time.getMinutes();
+  let clockDisplay = document.getElementById("testTime");
 
-window.addEventListener('load', function getBackgroundImage() {
+  // Time shows in console as a string
+  clockDisplay.innerText = currentTime; 
+}
 
-  console.log("starting request");
-
+function getBackgroundImage() {
   fetch('https://api.unsplash.com/photos/random')
-    .then(function(data) {
-      console.log(data)
-    // Here you get the data to modify as you please
-        // document.body.style.backgroundImage = data;
-        // getting a 401 error - no API key
-      })
-    })
-    .catch(function(error) {
-      console.log(error)
-    // If there is any error you will catch them here
-    });   
+  .then(function(data) {
+    console.log(data)
+  // Here you get the data to modify as you please
+      // document.body.style.backgroundImage = data;
+      // getting a 401 error - no API key
+    })  
 
-    // getBackgroundImage()
+  .catch(function(error) {
+    console.log(error)
+  // If there is any error you will catch them here
+  });   
+}
 
-    // do it all in one onload function in JS file
+// When the page is loaded/refreshed
+window.onload = function display () {
+  // Everytime you add a new function make sure to call it down here
+  showTime();
+  getBackgroundImage();
+}
+
+
+// do it all in one onload function in JS file
     
