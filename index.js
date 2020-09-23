4b288ae199bdf0e40de845ec116854fd4d18ad75
katrinamarquez <katrina.l.marquez@gmail.com>
2020-09-23 12:20:55 +1000

    Update with weather and HTML. Good one

--- index.js
+++ index.js
@@ -63,6 +63,30 @@ function writeGoal(){
   goalForm.innerHTML = "";
 }
 
+function weatherBalloon(cityID) {
+  // get cityID dynamically somehow
+  // let cityID = 'https://api.openweathermap.org/data/2.5/weather?id=6167865&appid=a9e0348fc474f2a0b75e00ca777a4429'
+  let key = 'a9e0348fc474f2a0b75e00ca777a4429';
+  fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+  .then(function(resp) { return resp.json() }) // Convert data to json
+  .then(function(data) {
+    console.log(data);
+    drawWeather(data);
+  })
+  .catch(function() {
+    // catch any errors
+  });
+}
+
+function drawWeather( d ) {
+    var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+    // var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+    
+    document.getElementById('description').innerHTML = d.weather[0].description;
+    document.getElementById('temp').innerHTML = celcius + '&deg;';
+    document.getElementById('location').innerHTML = d.name;
+}
+
 // do it all in one onload function in JS file
 // When the page is loaded/refreshed, calls all funcs
 window.onload = function display () {
@@ -70,4 +94,5 @@ window.onload = function display () {
   showTime();
   getQuote();
   getBackgroundImage();
+  weatherBalloon(2174003);
 }
